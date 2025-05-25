0625b715003369ce575d21c3ce19c969e7624bfb
reddyz-avinash <avinashreddy.dongale@s.amity.edu>
2025-05-25 12:38:53 +0530

    D code added

--- file.py
+++ file.py
@@ -13,4 +13,24 @@ def setup_logger():
         format='%(asctime)s - %(levelname)s - %(message)s',
         level=logging.INFO
     )
+def demo_statistics():
+    from text_input import TextLoader
+    loader = TextLoader("sample.txt")
+    loader.read_file()
+    loader.clean_text()
 
+    ts = TextStats(loader.cleaned_text)
+    ts.tokenize_words()
+    ts.tokenize_sentences()
+    ts.tokenize_paragraphs()
+
+    print("Words:", ts.word_count())
+    print("Sentences:", ts.sentence_count())
+    print("Paragraphs:", ts.paragraph_count())
+    print("Top 10 Words:", ts.most_common_words())
+
+    ts.generate_wordcloud()
+    ts.export_report()
+
+if _name_ == "_main_":
+    demo_statistics()
